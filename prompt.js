261be89cd480f1ed8bd5ce35a2f8491ef1975e3a
AlexaEskina <aaleksa.po@gmail.com>
2024-02-17 08:58:03 +0200

    refactor

--- prompt.js
+++ prompt.js
@@ -1,16 +1,15 @@
 let correct = false
 
 do {
-    let num = prompt ("Скільки тобі років?");
+    let num = prompt ("Скільки тобі років?"); 
     console.log(typeof num)
     if (!isNaN(num) && !(num <= 0)) {
         correct = true;
         let x = num % 10;
         let y = num % 100;
+        console.log(x, y)
 
-        if (y >= 10 && y <= 20) {
-            console.log(num + " років");
-          } else if (x === 1) {
+         if (x === 1) {
             console.log(num + " рік");
           } else if (x >= 2 && x <= 4){
             console.log(num + " роки");
